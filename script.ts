fcc65d7c039e1b21927c7825da76b41ab030007f
bi151103 <dangphanminhphuctbag@gmail.com>
2026-05-04 01:45:21 +0700

    chore: update script to use DOM api to create profile info section

--- script.ts
+++ script.ts
@@ -219,13 +219,51 @@ function createProfileActionSection(): HTMLElement {
   return profileActionSection;
 }
 
+function createProfileInfoSection(): HTMLElement {
+  const profileInfoSection = document.createElement("section");
+  profileInfoSection.className = "py-[25px] px-15px bg-white";
+
+  const profileName = document.createElement("h1");
+  profileInfoSection.appendChild(profileName);
+  profileName.textContent = "Phuc Dang";
+  profileName.className = "text-emphasis-tx";
+
+  const profileHeadline = document.createElement("p");
+  profileInfoSection.appendChild(profileHeadline);
+  profileHeadline.textContent = "Tester";
+
+  const profileCompany = document.createElement("p");
+  profileInfoSection.appendChild(profileCompany);
+  profileCompany.textContent = "CODE LEAP";
+
+  const profileLocationAndConnectionContainer = document.createElement("p");
+  profileInfoSection.appendChild(profileLocationAndConnectionContainer);
+  profileLocationAndConnectionContainer.textContent = "Viet Nam";
+
+  const dotSeparatorBtwNameAndNumberOfConnection =
+    document.createElement("span");
+  profileLocationAndConnectionContainer.appendChild(
+    dotSeparatorBtwNameAndNumberOfConnection,
+  );
+  dotSeparatorBtwNameAndNumberOfConnection.className = "dot";
+
+  const connections = document.createElement("a");
+  profileLocationAndConnectionContainer.appendChild(connections);
+  connections.href = "./connection.html";
+  connections.textContent = "74 Connections";
+
+  return profileInfoSection;
+}
+
 const header = createHeader();
 const backgroundImgSection = createBackgroundImageSection();
 const profileActionSection = createProfileActionSection();
+const profileInfoSection = createProfileInfoSection();
 
 bodyEle.prepend(header);
 bodyEle.insertBefore(backgroundImgSection, header.nextSibling);
 bodyEle.insertBefore(profileActionSection, backgroundImgSection.nextSibling);
+bodyEle.insertBefore(profileInfoSection, profileActionSection.nextSibling);
 
 const overlayEle = document.querySelector("[data-id='overlay']") as HTMLElement;
 const addFeaturedOverlayEle = document.querySelector(
