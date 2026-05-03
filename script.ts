c8f6307e8e2c8902738c70dbfe3ad31ae2b37943
bi151103 <dangphanminhphuctbag@gmail.com>
2026-05-04 01:55:56 +0700

    chore: update script to use DOM api to create about section

--- script.ts
+++ script.ts
@@ -255,15 +255,47 @@ function createProfileInfoSection(): HTMLElement {
   return profileInfoSection;
 }
 
+function createAboutSection(): HTMLElement {
+  const aboutSection = document.createElement("section");
+  aboutSection.className = "mt-10px p-15px bg-white";
+
+  const headerContainer = document.createElement("div");
+  aboutSection.appendChild(headerContainer);
+  headerContainer.className = "flex";
+
+  const title = document.createElement("h2");
+  headerContainer.appendChild(title);
+  title.textContent = "About";
+
+  const editAboutLink = document.createElement("a");
+  headerContainer.appendChild(editAboutLink);
+  editAboutLink.href = "./connection.html";
+  editAboutLink.className = "ml-auto";
+
+  const editAboutLinkImg = document.createElement("img");
+  editAboutLink.appendChild(editAboutLinkImg);
+  editAboutLinkImg.src = "./images/icons8-edit-100.png";
+  editAboutLinkImg.className = "h-sm-img w-sm-img";
+
+  const aboutContent = document.createElement("p");
+  aboutSection.appendChild(aboutContent);
+  aboutContent.textContent = "As early of 2026";
+  aboutContent.className = "mt-10px";
+
+  return aboutSection;
+}
+
 const header = createHeader();
 const backgroundImgSection = createBackgroundImageSection();
 const profileActionSection = createProfileActionSection();
 const profileInfoSection = createProfileInfoSection();
+const aboutSection = createAboutSection();
 
 bodyEle.prepend(header);
 bodyEle.insertBefore(backgroundImgSection, header.nextSibling);
 bodyEle.insertBefore(profileActionSection, backgroundImgSection.nextSibling);
 bodyEle.insertBefore(profileInfoSection, profileActionSection.nextSibling);
+bodyEle.insertBefore(aboutSection, profileInfoSection.nextSibling);
 
 const overlayEle = document.querySelector("[data-id='overlay']") as HTMLElement;
 const addFeaturedOverlayEle = document.querySelector(
