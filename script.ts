b94aba0fe761ee50ce9d125ab3facbd662b07db8
bi151103 <dangphanminhphuctbag@gmail.com>
2026-05-03 21:24:31 +0700

    chore: update script to use DOM api to create background image section

--- script.ts
+++ script.ts
@@ -140,6 +140,30 @@ function createHeader() {
 }
 createHeader();
 
+function createBackgroundImageSection() {
+  const backgroundImageSection = document.createElement("section");
+  backgroundImageSection.className = "relative";
+
+  const backgroundImg = document.createElement("img");
+  backgroundImg.src = "./images/background-image-cut.jpg";
+  backgroundImg.className = "w-full bg-white align-middle";
+  backgroundImageSection.appendChild(backgroundImg);
+
+  const settingLink = document.createElement("a");
+  settingLink.href = "./my-preferences.html";
+  backgroundImageSection.appendChild(settingLink);
+
+  const settingLinkImg = document.createElement("img");
+  settingLinkImg.src = "./images/icons8-setting-100.png";
+  settingLinkImg.className =
+    "w-[25px] aspect-square absolute top-0 right-0 mr-[15px] mt-[15px] bg-white rounded-full p-5px";
+  settingLink.appendChild(settingLinkImg);
+
+  bodyEle.prepend(backgroundImageSection);
+}
+
+createBackgroundImageSection();
+
 const overlayEle = document.querySelector("[data-id='overlay']") as HTMLElement;
 const addFeaturedOverlayEle = document.querySelector(
   "[data-id='overlay'] [data-id='add-featured-overlay']",
