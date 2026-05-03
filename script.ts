b6dceca808c3fd234b59f71f52742db30a9d5c0a
bi151103 <dangphanminhphuctbag@gmail.com>
2026-05-04 03:04:50 +0700

    chore: update script to use DOM api to create activity section and see all activity link

--- script.ts
+++ script.ts
@@ -374,6 +374,67 @@ function createPrivateToYouSection(): HTMLElement {
   return privateToYouSection;
 }
 
+function creataActivitySection(): HTMLElement {
+  const activitySection = document.createElement("section");
+  activitySection.className = "mt-10px p-15px bg-white";
+
+  const title = document.createElement("h2");
+  activitySection.appendChild(title);
+  title.textContent = "Activity";
+
+  const follower = document.createElement("p");
+  activitySection.appendChild(follower);
+  follower.textContent = "74 followers";
+  follower.className = "text-xs-small";
+
+  const activityLink = document.createElement("a");
+  activitySection.appendChild(activityLink);
+  activityLink.href = "./feed-activity.html";
+  activityLink.className = "w-full flex mt-10px text-inherit";
+
+  const activityThumb = document.createElement("img");
+  activityLink.appendChild(activityThumb);
+  activityThumb.src = "./images/liverpool.jpg";
+  activityThumb.className = "h-md-img w-md-img object-cover";
+
+  const activityRight = document.createElement("div");
+  activityLink.appendChild(activityRight);
+  activityRight.className = "pl-10px";
+
+  const activityContent = document.createElement("p");
+  activityRight.appendChild(activityContent);
+  activityContent.textContent = `LFC and adidas have unveiled their new Bringback range, with a
+            collection inspired by the club's legendary 1995/96 away kit. At the
+            heart of the Bringback range is a reissue of the club's iconic
+            1995/96 away shirt, reconnecting supporters with one of the most
+            recognisable designs in the club's history. The collection is
+            supported by a full archive-inspired training wear offering,
+            including classic silhouettes such as shorts, a sweatshirt, drill
+            top, drill pants and T-shirt. Each piece returns in the unmistakable
+            green, black and white colour palette synonymous with the era, while
+            heritage details including the original Liverpool FC crest from the
+            1995/96 season, Carlsberg sponsor and bold adidas branding complete
+            the authentic 90s aesthetic.`;
+  activityContent.className = "text-emphasis-tx line-clamp-2";
+
+  const activityReactHistory = document.createElement("p");
+  activityRight.appendChild(activityReactHistory);
+  activityReactHistory.textContent = "You liked this";
+  activityReactHistory.className = "text-xs-small";
+
+  return activitySection;
+}
+
+function createActivitySeeAllButton(): HTMLElement {
+  const seeAllBtnLink = document.createElement("a");
+  seeAllBtnLink.href = "./profile-activity.html";
+  seeAllBtnLink.textContent = "See all";
+  seeAllBtnLink.className =
+    "block w-full leading-loose bg-white border-t border-separator-line hover:bg-primary-btn-hover-bg text-center";
+
+  return seeAllBtnLink;
+}
+
 const header = createHeader();
 const backgroundImgSection = createBackgroundImageSection();
 const profileActionSection = createProfileActionSection();
@@ -381,6 +442,8 @@ const profileInfoSection = createProfileInfoSection();
 const aboutSection = createAboutSection();
 const featuredSection = createFeaturedSection();
 const privateToYouSection = createPrivateToYouSection();
+const activitySection = creataActivitySection();
+const seeAllBtnLink = createActivitySeeAllButton();
 
 const bodyEle = document.querySelector("body") as HTMLElement;
 bodyEle.className = "py-50px text-small";
@@ -391,6 +454,8 @@ bodyEle.insertBefore(profileInfoSection, profileActionSection.nextSibling);
 bodyEle.insertBefore(aboutSection, profileInfoSection.nextSibling);
 bodyEle.insertBefore(featuredSection, aboutSection.nextSibling);
 bodyEle.insertBefore(privateToYouSection, featuredSection.nextSibling);
+bodyEle.insertBefore(activitySection, privateToYouSection.nextSibling);
+bodyEle.insertBefore(seeAllBtnLink, activitySection.nextSibling);
 
 const overlayEle = document.querySelector("[data-id='overlay']") as HTMLElement;
 const addFeaturedOverlayEle = document.querySelector(
